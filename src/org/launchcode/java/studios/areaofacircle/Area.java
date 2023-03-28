8c64bd769cc1079fbba4a1d9107ddafc68685f9e
emjayholla <maggie.hollander@gmail.com>
2023-03-27 19:57:28 -0500

    Completed studio, minus bonus mission

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,7 +5,7 @@ public class Area {
         Scanner areaOfACircle = new Scanner(System.in);
         System.out.println("Enter a radius for your circle:");
         Double radius = areaOfACircle.nextDouble();
-        System.out.println("The area of your circle is " + (3.14*radius*radius) + "!");
+        System.out.println("The area of your circle is " + (Circle.getArea(radius)) + "!");
     }
 
 }
